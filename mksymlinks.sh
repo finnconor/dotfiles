860948ac9bed12a7f84c1778dd4f089736c28d26
Conor Finn <finnconor@gmail.com>
2025-08-25 15:45:17 +1000

    Added shell script to make the symlinks

---
+++ mksymlinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Variables
+
+dir=~/.dotfiles
+
+# Links
+
+ln -s $dir/.xinitrc ~/.xinitrc
+ln -s $dir/.zshrc ~/.zshrc
+ln -s $dir/.zsh_history ~/.zsh_history
+ln -s $dir/.emacs ~/.emacs
+ln -s $dir/.dir_colors ~/.dir_colors
