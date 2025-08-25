b7fbaa3c490557e6e3ae760668dccf288ab4d3ac
Conor Finn <finnconor@gmail.com>
2025-08-25 17:29:50 +1000

    Added i3 config

--- mksymlinks.sh
+++ mksymlinks.sh
@@ -7,7 +7,12 @@ dir=~/.dotfiles
 # Links
 
 ln -s $dir/xinitrc ~/.xinitrc
+
 ln -s $dir/zshrc ~/.zshrc
 ln -s $dir/oh-my-zsh ~/.oh-my-zsh
+
 ln -s $dir/emacs ~/.emacs
+
 ln -s $dir/dir_colors ~/.dir_colors
+
+ln -s $dir/i3/config ~/.i3/config
